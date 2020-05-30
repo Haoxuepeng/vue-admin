c8ecc68a768feb9342cbd2427021418f2019e0e8
郝学鹏 <523492996@qq.com>
2020-05-30 15:21:37 +0800

    配置路由

---
+++ src/views/Login/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="login">
+      登录
+  </div>
+</template>
+
+<script>
+
+  export default {
+   name: 'Login',
+    props:{},
+    data () {
+      return {
+
+      };
+    },
+
+    components: {},
+
+    computed: {},
+
+    beforeMount() {},
+
+    mounted() {},
+
+    methods: {},
+
+    watch: {}
+
+  }
+
+</script>
+<style  scoped lang='scss'>
+#login{
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
